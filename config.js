606ddf0c9bb677a42924c75b89dbc7f9d5ce6f87
saiyien <61818036+saiyien@users.noreply.github.com>
2023-08-14 06:01:51 +0800

    update config

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 // config.js
 module.exports = {
-    databaseUrl: process.env.MONGODB_URI || 'mongodb+srv://icon:iCon100&@icondb.n39xt.mongodb.net/user-joborder-app?retryWrites=true&w=majority',    
-    //databaseUrl: process.env.MONGODB_URI || 'mongodb://localhost:27017/user-joborder-app',
+    databaseUrl: process.env.MONGODB_URI || 'mongodb://localhost:27017/user-joborder-app',
     jwtSecret: '123',
 };
